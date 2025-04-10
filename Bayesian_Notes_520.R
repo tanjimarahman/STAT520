e5cbe7e5dadb990a8b0f7167dad2c52f523a61be
Tanjima Rahman <tanjimarahman222@gmail.com>
2025-04-10 10:47:28 -0700

    Update Bayesian_Notes_520.R

--- Bayesian_Notes_520.R
+++ Bayesian_Notes_520.R
@@ -23,10 +23,10 @@ BBB<-function(n,p,alpha,beta)
  
 }
 
-n<-1
-p<-0.3
-alpha<-1
-beta<-1
+n<-10
+p<-0.5
+alpha<-2
+beta<-2
 
 BBB(n,p,alpha,beta)
 #Inference for Normal with Normal Prior
@@ -58,4 +58,4 @@ BNN<-function(n,mu,sigma,theta,tau)
   
 }
 
-BNN(10,20,5,12,4)
+BNN(12,22,6,13,5)
